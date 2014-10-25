239a570c4f3cf7a2f39df543d2c76c0d35f33099
Maciek Makowski <maciek.makowski@gmail.com>
2014-10-25 15:57:05 +0100

    cleanup: minor code style improvements

--- src/main/scala/com/mmakowski/maven/plugins/specs2/Specs2Runner.scala
+++ src/main/scala/com/mmakowski/maven/plugins/specs2/Specs2Runner.scala
@@ -1,19 +1,16 @@
 package com.mmakowski.maven.plugins.specs2
 
-import org.scalatools.testing.{Event, EventHandler, Logger}
-import org.scalatools.testing.Result
-import org.scalatools.testing.Result._
-import java.net.{URL, URLClassLoader}
 import java.io.File
-import org.apache.maven.artifact.Artifact
+import java.net.{URL, URLClassLoader}
+
 import org.apache.maven.plugin.logging.Log
 import org.apache.maven.project.MavenProject
-import scalax.file.{Path, PathMatcher}
+import org.scalatools.testing.{Event, EventHandler, Logger, Result}
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import scalax.file.Path
 import scalax.file.PathMatcher._
-
-import scala.collection.JavaConversions._
-
-import scala.collection.mutable.{Map}
 
 /**
  * Executes pre-compiled specifications found in test classes directory. 
@@ -25,15 +22,15 @@ class Specs2Runner(args: String) {
   /**
    * A handler that counts the total for each possible result of test when running a Spec
    */
-  private class AggregatingHandler extends EventHandler {
-    val testCounts: Map[String, Int] = Map() withDefaultValue 0
+  private class AggregatingEventHandler extends EventHandler {
+    val testCounts: mutable.Map[String, Int] = mutable.Map() withDefaultValue 0
     
     def handle(event: Event) {
       val resultType = event.result.toString
       testCounts(resultType) = testCounts(resultType) + 1  
     }
     
-    def report = Result.values.map(_.toString).map(s => s + ": " + testCounts(s)).mkString(" "*3)
+    def report = Result.values.map(s => s"$s: ${testCounts(s.toString)}").mkString(", ")
 
     def noErrorsOrFailures = testCounts("Error") + testCounts("Failure") == 0
   }
@@ -51,30 +48,30 @@ class Specs2Runner(args: String) {
   }
   
   def runSpecs(log: Log, project: MavenProject, suffix: String): java.lang.Boolean = {
-    val classpath = urlsOf(List() ++ project.getTestClasspathElements().asInstanceOf[java.util.List[String]])
+    val classpath = urlsOf(project.getTestClasspathElements.asInstanceOf[java.util.List[String]].asScala)
     log.debug("test classpath: " + classpath)
-    val fullSuffix = "%s.class" format suffix
-    log.debug("searching for specs ending in %s" format fullSuffix)
+    val fullSuffix = s"$suffix.class"
+    log.debug(s"searching for specs ending in $fullSuffix")
     val testClassesPath = Path.fromString(project.getBuild.getTestOutputDirectory)
     val specs = findSpecsIn(testClassesPath, "", _.name.endsWith(fullSuffix))
     val classLoader = new URLClassLoader(classpath.toArray[URL], getClass.getClassLoader)
     val runWithTestClassLoader = runWithClassLoader(classLoader, log)_
     
     val runner = new TestInterfaceRunner(classLoader, log)
-    
+
     def runSpec(failingSpecs: Seq[String], spec: String): Seq[String] = {
       log.info(spec + ":")
-      val handler = new AggregatingHandler
-      val runCompleted = runWithTestClassLoader("spec runner", runner.runSpecification(spec, handler, args.split(" ")))
-      log.info(handler.report)
+      val eventHandler = new AggregatingEventHandler
+      val runCompleted = runWithTestClassLoader("spec runner", runner.runSpecification(spec, eventHandler, args.split(" ")))
+      log.info(eventHandler.report)
 
-      val result = runCompleted && handler.noErrorsOrFailures
+      val result = runCompleted && eventHandler.noErrorsOrFailures
       if (!result) failingSpecs :+ spec else failingSpecs
     }
     
-    def generateIndex() = {
+    def generateIndex(): Boolean = {
       val indices = findSpecsIn(testClassesPath, "", _.name == "index.class")
-      if (indices isEmpty) true else {
+      if (indices.isEmpty) true else {
         val index = indices(0)
         log.info("generating " + index)
         runWithTestClassLoader("index generator", new HtmlRunner(classLoader).start(index))
@@ -87,11 +84,11 @@ class Specs2Runner(args: String) {
     failures.isEmpty && indexGenerationSuccesful
   }
   
-  def url(file: File) = new URL(file.getAbsoluteFile.toURI.toASCIIString)
+  private def url(file: File) = new URL(file.getAbsoluteFile.toURI.toASCIIString)
 
-  def urlsOf(files: List[String]) = files.map(new File(_)).map(url(_))
+  private def urlsOf(files: Seq[String]) = files.map(new File(_)).map(url)
 
-  def findSpecsIn(dir: Path, pkg: String, pred: Path => Boolean): Seq[String] = {
+  private def findSpecsIn(dir: Path, pkg: String, pred: Path => Boolean): Seq[String] = {
     def qualified(name: String) = if (pkg.isEmpty) name else pkg + "." + name
     dir.children().toSeq.collect {
       case IsFile(f) if pred(f) => Seq(qualified(f.name.take(f.name.length - ".class".length))) 
@@ -99,7 +96,7 @@ class Specs2Runner(args: String) {
     }.flatten
   }
   
-  def runWithClassLoader[T](classLoader: ClassLoader, log: Log)(threadName: String, body: => T): Boolean = {
+  private def runWithClassLoader[T](classLoader: ClassLoader, log: Log)(threadName: String, body: => T): Boolean = {
     object runner extends Runnable {
       var success = false
       def run() = {
@@ -118,14 +115,14 @@ class Specs2Runner(args: String) {
     runner.success
   }
 
-  def logFailingSpecs(failures: Seq[String], log: Log) =
-    if (!failures.isEmpty) log.info(failures.mkString("\n\nSpecs Failing or In Error:\n", "\n", "\n\n"))
+  private def logFailingSpecs(failures: Seq[String], log: Log) =
+    if (failures.nonEmpty) log.info(failures.mkString("\n\nSpecs Failing or In Error:\n", "\n", "\n\n"))
 
   // Wrappers for specs2 runners that invoke relevant methods via reflection. This approach allows the plug-in to not depend on 
   // specific version of specs2 and use whatever version has been specified as the project's dependency -- as long, as it provides
   // runners with signatures expected by this plug-in.
     
-  class TestInterfaceRunner(classLoader: ClassLoader, log: Log) {
+  private class TestInterfaceRunner(classLoader: ClassLoader, log: Log) {
     val RunnerClassName = "org.specs2.runner.TestInterfaceRunner"
     val runnerClass = classLoader.loadClass(RunnerClassName)
     val runner = runnerClass.getConstructor(classOf[ClassLoader], classOf[Array[Logger]]).newInstance(classLoader, Array(new DebugLevelLogger(log)))
@@ -135,7 +132,7 @@ class Specs2Runner(args: String) {
       runSpecificationMethod.invoke(runner, spec, handler, modes)
   }
   
-  class HtmlRunner(classLoader: ClassLoader) {
+  private class HtmlRunner(classLoader: ClassLoader) {
     val RunnerClassName = "org.specs2.runner.HtmlRunner"
     val runnerClass = classLoader.loadClass(RunnerClassName)
     val runner = runnerClass.getConstructor().newInstance()
